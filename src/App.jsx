ddc79cd88a5237877f4f5ef5a285efbb7d14b99c
Papa403 <alilykai@gmail.com>
2025-08-24 21:20:35 -0500

    Add login functionality and update notes service to handle authentication

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Footer from './components/Footer'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import noteService from './services/notes'
-
+import loginService from './services/login'
 
 
 
@@ -12,6 +12,9 @@ const App = () => {
   const [newNote,setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService
@@ -68,10 +71,68 @@ const App = () => {
       })
   }
 
+  const handleLogin = async event => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      noteService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>
+          username
+          <input
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          password
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange} />
+      <button type="submit">save</button>
+    </form>
+  )
+
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      
+      {!user && loginForm()}
+      {user && (
+      <div>
+        <p>{user.name} logged in</p>
+        {noteForm()}
+      </div>
+    )}
+
       <div>
         <button onClick={()=>setShowAll(!showAll)}>
           show {showAll ? 'important':'all'}
